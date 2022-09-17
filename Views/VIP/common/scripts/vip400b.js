76d9442498f90d928e13e1f5bac429a5f9d02305
QQgalaxy99 <113761101+QQgalaxy99@users.noreply.github.com>
2022-09-17 15:05:52 +0700

    Add files via upload

---
+++ Views/VIP/common/scripts/vip400b.js
@@ -0,0 +1,12 @@
+﻿function popLiveChat(url) {
+    event.preventDefault();
+    var chat;
+    var w = 475;
+    var h = 465;
+    var winX = (screen.availWidth - w) / 2;
+    var winY = 50;
+    var paras = 'width=' + w + 'px,height=' + h + 'px,left=' + winX + 'px,top=' + winY + 'px,scrollbars=yes,resizable=yes';
+    chat = window.open(url, 'LiveChat', paras);
+    chat.focus();
+    return false;
+}
